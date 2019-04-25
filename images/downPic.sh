1b1817c35de7225850785e66cfbca3ef94bc2f57
guankai <guankai1984@126.com>
2019-04-25 08:46:40 +0800

    hexo deploy

---
+++ images/downPic.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+wget https://ws1.sinaimg.cn/large/006tNbRwly1fwou1h7ueqj319y074qbn.jpg
+wget https://ws3.sinaimg.cn/large/006tNbRwly1fwou28laspj319w02adi2.jpg
+wget https://ws1.sinaimg.cn/large/006tNbRwly1fwou2ryn6bj31a003adiy.jpg
+wget https://ws2.sinaimg.cn/large/006tNbRwly1fwou3998swj319c0natdb.jpg
+wget https://ws3.sinaimg.cn/large/006tNbRwly1fwou3sson0j31c80ssn4v.jpg
+wget https://ws4.sinaimg.cn/large/006tNbRwly1fwou43vq5ej319w06cq5n.jpg
+wget https://ws1.sinaimg.cn/large/006tNbRwly1fwou80s0crj31a40awgs7.jpg
+wget https://ws3.sinaimg.cn/large/006tNbRwly1fwou97i58dj319w04sn1q.jpg
+wget https://ws2.sinaimg.cn/large/006tNbRwly1fwoua3g61vj319y052dj9.jpg
+wget https://ws3.sinaimg.cn/large/006tNbRwly1fwouah7n4qj319y07wjxz.jpg
+wget https://ws2.sinaimg.cn/large/006tNbRwly1fwouaxm5mfj31a20kyn9r.jpg
+wget https://ws2.sinaimg.cn/large/006tNbRwly1fwoubam2rhj319y04wjvw.jpg
+wget https://ws3.sinaimg.cn/large/006tNbRwly1fwoubqli8dj319w08awlj.jpg
